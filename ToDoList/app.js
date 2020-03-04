babb9dd8f2b1ec02958d52105e80815ffe694f20
TanishqSingla <tanishqsingla00@gmail.com>
2020-03-04 13:25:36 +0530

    code improvement

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -8,32 +8,17 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   let today = new Date();
   let currentDay = today.getDay();
-  let day = "";
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-  }
-
-  res.render("index", { day: day });
+  let day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+  res.render("index", { day: day[currentDay] });
 });
 
 app.listen("5000", () => {
