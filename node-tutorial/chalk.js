6efb020cc81f7670097d68dde2242a96e3e4e3cb
TanishqSingla <tanishqsingla00@gmail.com>
2020-03-17 20:52:37 +0530

    first module

---
+++ node-tutorial/chalk.js
@@ -0,0 +1,4 @@
+// This is the first line in apps using node, the below code shows the usage of chalk
+const chalk = require('chalk')
+
+console.log(chalk.blue.inverse.bold('Success'))
---
+++ node-tutorial/uInput.js
@@ -0,0 +1,10 @@
+// This file shows how to get user input
+const chalk = require('chalk')
+
+const argument = process.argv[2];
+
+if(argument === 'add') {
+    console.log('Added note!');
+} else if (argument === 'remove'){ 
+    console.log('Removed note!');
+}
