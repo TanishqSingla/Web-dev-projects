10559a7a17ba6fc4c2b2030f5a9c9be014fa7703
TanishqSingla <tanishqsingla00@gmail.com>
2020-03-22 11:16:15 +0530

    made list function

--- node-tutorial/notes-app/app.js
+++ node-tutorial/notes-app/app.js
@@ -4,54 +4,54 @@ const notes = require("./notes.js");
 yargs.version("0.0.1");
 
 yargs.command({
-  command: "add",
-  describe: "Adds a new note",
-  builder: {
-    title: {
-      describe: "Note Title",
-      demandOption: true,
-      type: "string"
-    },
-    body: {
-      describe: "Content",
-      demandOption: true,
-      type: "string"
-    }
-  },
-  handler: function(argv) {
-    notes.addNote(argv.title, argv.body);
-  }
+	command: "add",
+	describe: "Adds a new note",
+	builder: {
+		title: {
+			describe: "Note Title",
+			demandOption: true,
+			type: "string"
+		},
+		body: {
+			describe: "Content",
+			demandOption: true,
+			type: "string"
+		}
+	},
+	handler: function(argv) {
+		notes.addNote(argv.title, argv.body);
+	}
 });
 
 yargs.command({
-  command: "remove",
-  describe: "Removes a new note",
-  builder: {
-    title: {
-      describe: 'used as --title=""',
-      demandOption: true,
-      type: "string"
-    }
-  },
-  handler: function(argv) {
-    notes.rmNote(argv.title);
-  }
+	command: "remove",
+	describe: "Removes a new note",
+	builder: {
+		title: {
+			describe: 'used as --title=""',
+			demandOption: true,
+			type: "string"
+		}
+	},
+	handler: function(argv) {
+		notes.rmNote(argv.title);
+	}
 });
 
 yargs.command({
-  command: "list",
-  describe: "Lists the notes",
-  handler: function() {
-    console.log("Listed a note");
-  }
+	command: "list",
+	describe: "Lists the notes",
+	handler: function() {
+		notes.listNote();
+	}
 });
 
 yargs.command({
-  command: "read",
-  describe: "Reads a note",
-  handler: function() {
-    console.log("Read a note");
-  }
+	command: "read",
+	describe: "Reads a note",
+	handler: function() {
+		console.log("Read a note");
+	}
 });
 
 yargs.parse();
--- node-tutorial/notes-app/notes.js
+++ node-tutorial/notes-app/notes.js
@@ -3,57 +3,65 @@ const fs = require("fs");
 const chalk = require("chalk");
 
 const getNotes = function() {
-  return "Your Notes...";
+	return "Your Notes...";
 };
 
 //* addNote method for the app
 const addNote = function(title, body) {
-  const notes = loadNote();
-  const duplicate = notes.filter(note => note.title === title);
+	const notes = loadNote();
+	const duplicate = notes.filter(note => note.title === title);
 
-  if (duplicate.length === 0) {
-    notes.push({
-      title: title,
-      body: body
-    });
+	if (duplicate.length === 0) {
+		notes.push({
+			title: title,
+			body: body
+		});
 
-    saveNotes(notes);
+		saveNotes(notes);
 
-    console.log(chalk.green.inverse("New note added"));
-  } else {
-    console.log(chalk.red.inverse("Note title taken"));
-  }
+		console.log(chalk.green.inverse("New note added"));
+	} else {
+		console.log(chalk.red.inverse("Note title taken"));
+	}
 };
 
 const rmNote = function(title) {
-  const notes = loadNote();
-  const notesToKeep = notes.filter(note => note.title !== title);
+	const notes = loadNote();
+	const notesToKeep = notes.filter(note => note.title !== title);
 
-  if (notes.length > notesToKeep.length) {
-    console.log(chalk.green.inverse("Note removed"));
-  } else {
-    console.log(chalk.red.inverse("No note found"));
-  }
+	if (notes.length > notesToKeep.length) {
+		console.log(chalk.green.inverse("Note removed"));
+	} else {
+		console.log(chalk.red.inverse("No note found"));
+	}
+};
+
+const listNote = () => {
+	const notes = loadNote();
+	for (let note = 0; note < notes.length; note += 1) {
+		console.log(notes[note].title);
+	}
 };
 
 const loadNote = function() {
-  try {
-    const dataBuffer = fs.readFileSync("notes.json");
-    const dataJSON = dataBuffer.toString();
-    return JSON.parse(dataJSON);
-  } catch (e) {
-    return [];
-  }
+	try {
+		const dataBuffer = fs.readFileSync("notes.json");
+		const dataJSON = dataBuffer.toString();
+		return JSON.parse(dataJSON);
+	} catch (e) {
+		return [];
+	}
 };
 
 const saveNotes = function(notes) {
-  const dataJSON = JSON.stringify(notes);
-  fs.writeFileSync("notes.json", dataJSON);
+	const dataJSON = JSON.stringify(notes);
+	fs.writeFileSync("notes.json", dataJSON);
 };
 
 //? Exporting module
 module.exports = {
-  addNote: addNote,
-  getNotes: getNotes,
-  rmNote: rmNote
+	addNote: addNote,
+	getNotes: getNotes,
+	rmNote: rmNote,
+	listNote: listNote
 };
