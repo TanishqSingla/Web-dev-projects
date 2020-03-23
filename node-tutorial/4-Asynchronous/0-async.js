19f9b2d2b14a5b1b31bf39333aa23291b31608fb
TanishqSingla <tanishqsingla00@gmail.com>
2020-03-23 22:04:05 +0530

    adding content

---
+++ node-tutorial/4-Asynchronous/0-async.js
@@ -0,0 +1,13 @@
+//This file is a walkthrough of order of different tasks done
+
+console.log("Starting");
+
+setTimeout(() => {
+  console.log("2 second timer");
+}, 2000);
+
+setTimeout(() => {
+  console.log("0 second timer");
+}, 0);
+
+console.log("Stopping");
